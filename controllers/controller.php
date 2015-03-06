8c23497cf9706e81301ff401463edcf18c0c7e3e
jaindeepali <djdeepalijain811@gmail.com>
2015-03-06 17:32:19 +0530

    basic framework

---
+++ controllers/controller.php
@@ -0,0 +1,12 @@
+<?php
+
+	/*Main controller file*/
+	class controller{
+
+		//Function to render home.php
+		public function home()
+		{
+			return render('home.php');
+		}
+	}
+?>
---
+++ views/layout.php
@@ -0,0 +1,11 @@
+<!doctype html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title><?=$title?></title>
+	</head>
+	<body>
+	</body>
+</html>
