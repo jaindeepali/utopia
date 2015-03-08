c73e7c1c8048ad0125cfb7684ada5d0d1f2322d5
jaindeepali <djdeepalijain811@gmail.com>
2015-03-08 21:24:20 +0530

    Insight.php

--- views/insights.php
+++ views/insights.php
@@ -4,7 +4,34 @@
 		Prototype using demo data
 	</div>
 	<br/><br/><br/><br/>
-	<div class="row-fluid content">
-		
+	<div class="row-fluid content" style="text-align:center;">
+		<table class="table insight-table">
+			<tr>
+				<td style="text-align:right">Motivation:</td>
+				<td style="text-align:left"><span class="pos" style="padding:0px 60px"> </span><span class="neg" style="padding:0px 40px"> </span> &nbsp; 60%</td>
+			</tr>
+			<tr>
+				<td style="text-align:right">Commitment to deadlines:</td>
+				<td style="text-align:left"><span class="pos" style="padding:0px 52px"> </span><span class="neg" style="padding:0px 48px"> </span> &nbsp; 52%</td>
+			</tr>
+			<tr>
+				<td style="text-align:right">Regularity:</td>
+				<td style="text-align:left"><span class="pos" style="padding:0px 80px"> </span><span class="neg" style="padding:0px 20px"> </span> &nbsp; 80</td>
+			</tr>
+			<tr>
+				<td style="text-align:right">Procastination:</td>
+				<td style="text-align:left"><span class="pos" style="padding:0px 78px"> </span><span class="neg" style="padding:0px 22px"> </span> &nbsp; 78%</td>
+			</tr>
+			<tr>
+				<td style="text-align:right">Focus on one goal:</td>
+				<td style="text-align:left"><span class="pos" style="padding:0px 37px"> </span><span class="neg" style="padding:0px 63px"> </span> &nbsp; 37%</td>
+			</tr>
+		</table>
+		<h4>Synopsis</h4>
+		<ul>
+			<li>You tend to procastinate and work mostly near the deadline.</li><br>
+			<li>Your work output increases just before the deadline, even exceeds the planned effort.</li><br>
+			<li>You tend to find it difficult to stick to schedule but manage to accomplish tasks under pressure.</li><br>
+		</ul>
 	</div>
 </div>
